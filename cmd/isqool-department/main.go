04ca192fc4aba49d9be5f458b0d8bedb418ca8a9
Rothanak So <rothanak.hello@gmail.com>
2019-12-31 19:47:29 -0500

    Implement full sync

--- cmd/isqool-department/main.go
+++ cmd/isqool-department/main.go
@@ -24,31 +24,53 @@ func main() {
 	c.CacheDir = userCacheDir + "/isqool/web-cache"
 	c.AllowURLRevisit = true
 
-	deptTable, err := scrape.GetDepartment(c, "Spring 2019", 6502)
+	seedTerm := "Spring 2020"
+	deptId := 6502
+
+	// Scrape the first term as a starting point
+	initialDept, err := scrape.GetDepartment(c, seedTerm, deptId)
 	if err != nil {
 		panic(err)
 	}
 
 	seen := make(map[string]bool)
 	var courses []string
-	for _, row := range deptTable {
+	for _, row := range initialDept {
 		if _, found := seen[row.Name]; !found {
 			courses = append(courses, row.Name)
 			seen[row.Name] = true
 		}
 	}
 
+	// Scrape all the courses offered that term
 	var isqTable []scrape.CourseIsq
 	var gradesTable []scrape.CourseGrades
-
 	for _, course := range courses {
 		isqs, grades, err := scrape.GetIsqAndGrades(c.Clone(), course, false)
 		if err != nil {
 			panic(err)
 		}
-
 		isqTable = append(isqTable, isqs...)
 		gradesTable = append(gradesTable, grades...)
+	}
+
+	seen = make(map[string]bool)
+	var terms []string
+	for _, row := range isqTable {
+		if _, found := seen[row.Term]; !found && row.Term != seedTerm {
+			terms = append(terms, row.Term)
+			seen[row.Term] = true
+		}
+	}
+
+	// Scrape all the terms those courses were offered in
+	deptTable := initialDept
+	for _, term := range terms {
+		dept, err := scrape.GetDepartment(c, term, deptId)
+		if err != nil {
+			panic(err)
+		}
+		deptTable = append(deptTable, dept...)
 	}
 
 	// Connect to BigQuery
@@ -57,7 +79,7 @@ func main() {
 		panic(fmt.Errorf("failed to connect to bigquery: %v", err))
 	}
 
-	// Insert (merge) the department schedule, isqs, and grades
+	// Insert (merge) the department schedules, isqs, and grades
 	if err := bq.InsertDepartments(deptTable); err != nil {
 		panic(fmt.Errorf("failed to insert department schedule: %v", err))
 	}
